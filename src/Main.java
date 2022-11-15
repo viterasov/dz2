953bd6a2294c707f26b6bc2519da5fc8b358bbee
unknown <C:\Users\1\AppData\Roaming\The Bat!>
2022-11-15 21:10:12 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+public class Main {
+    public static void main(String[] args) {
+byte a = 1;
+System.out.println(1);
+short b = 150;
+System.out.println(b);
+int c = 50000;
+System.out.println(c);
+long d = 1234567890;
+System.out.println(d);
+double e = 1.5;
+System.out.println(e);
+float fish =5;
+float onePotoin = fish / 3;
+System.out.println(onePotoin);
+double weightFirstBoxer = 78.2;
+double weightSecondBoxer = 82.7;
+double totalWeight = weightFirstBoxer + weightSecondBoxer;
+System.out.println("Щбщий вес бойцов " + totalWeight + "кг" );
+weightFirstBoxer = 78.2;
+weightSecondBoxer = 82.7;
+        double WeightDifference = weightSecondBoxer - weightFirstBoxer;
+        System.out.println("Разница в весе " + WeightDifference + "кг" );
+        int weightOneBanana = 80;
+        int weightOnePortionMilk = 105;
+        int weightOnePortionIcecream = 100;
+        int oneEgg =70;
+        int Bananas = 5;
+        int milkPortiion =2;
+        int icecreamPortion =2;
+        int eggs =4;
+        int totalWeightBananas = weightOneBanana * Bananas;
+        int totalWeightMilk = weightOnePortionMilk * milkPortiion;
+        int totalWeightIcecream = weightOnePortionIcecream * icecreamPortion;
+        int totalWeightEggs = oneEgg * eggs;
+        int totalWeightProducts = totalWeightBananas + totalWeightMilk + totalWeightIcecream + totalWeightEggs;
+System.out.println("Общий вес продуктов " +totalWeightProducts + " грамм");
+            float totalWeightgramm = 1090;
+            float oneKg = 1000;
+float totalWeightKg = totalWeightgramm / oneKg;
+System.out.println("Вес в килограммах " + totalWeightKg);
+double Delet =7;
+            double dietaFirst =0.25;
+            double dietaSecond =0.500;
+            double dietaFirstDays = Delet / dietaFirst;
+            double dietaSecondDays = Delet / dietaSecond;
+            double dietaAverageDays = (dietaFirstDays + dietaSecondDays) / 2;
+            System.out.println(" При первой диете " + dietaFirstDays + " дней");
+            System.out.println(" При второй диете " + dietaSecondDays + " дней");
+            System.out.println("средняя диета " +dietaAverageDays + " дней");
+
+        float MariaMonth =67760;
+        float DenisMonth =83690;
+        float KristinaMonth =76230;
+        float profitMariaMonth = MariaMonth * 1.1f;
+        float profitDenisMonth = DenisMonth * 1.1f;
+        float profitKristinaMonth = KristinaMonth * 1.1f;
+        float MariaYear = MariaMonth * 12;
+        float DenisYear = DenisMonth * 12;
+        float KristinaYear = KristinaMonth *12;
+        float MariaYearProfit = profitMariaMonth *12;
+        float DenisYearProfit = profitDenisMonth *12;
+        float KristinaYearProfit = profitKristinaMonth *12;
+        float differenceYearMaria = MariaYearProfit - MariaYear;
+        float differenceYearDenis = DenisYearProfit - DenisYear;
+        float differenceYearKristina = KristinaYearProfit - KristinaYear;
+            System.out.println(" Маша теперь получает " + profitMariaMonth + " в месяц");
+            System.out.println(" Годовой доход вырос на " + differenceYearMaria + " рублей");
+            System.out.println(" Денис теперь получает " + profitDenisMonth + " в месяц");
+            System.out.println(" Годовой доход вырос на " + differenceYearDenis + " рублей");
+            System.out.println(" Кристина теперь получает " + profitKristinaMonth + " в месяц");
+            System.out.println(" Годовой доход вырос на " +differenceYearKristina + " рублей" );
+
+             a =12;
+             b =27;
+             c =44;
+             d =15;
+             e =9;
+            double  result = a * ( b + ( c -d * e));
+            System.out.println(result);
+            double invert = result * - 1;
+            System.out.println(invert);
+
+            a = 5;
+            b = 7;
+            double a1 = (a * b) / a;
+            double b1 = (a * b) / b;
+            System.out.println("a = " + a1);
+            System.out.println("b = " + b1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
